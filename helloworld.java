4c9c61f5b067603dec46775141482b80208f4181
Mahesh-Bhangale <80506852+Mahesh-Bhangale@users.noreply.github.com>
2023-01-18 09:33:00 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Checking jenkins automation and progamming ouput by  gbgv");  
+     System.out.println("Checking jenkins automation and progamming ouput by mahesh");  
     }  
 }  
